3713d31126a7bc7e2b6060e6628a8997750522e4
jtrentes <help@genmymodel.com>
2014-06-26 12:18:59 +0200

    commit by jtrentes using genmymodel: v20140626_121858

---
+++ model.py
@@ -0,0 +1,27 @@
+class MyClass(object):
+	def __init__(self):
+		self.myClass2 = None
+		
+	# Start of user code -> properties/constructors for MyClass class
+
+	# End of user code
+	# Start of user code -> methods for MyClass class
+
+	# End of user code
+
+class MyClass2(object):
+	def __init__(self):
+		self.myClass = None
+		
+	# Start of user code -> properties/constructors for MyClass2 class
+
+	# End of user code
+	# Start of user code -> methods for MyClass2 class
+
+	# End of user code
+
+
+
+# Start of user code -> functions/methods for model package
+
+# End of user code
